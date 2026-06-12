643180572fa773d0db0efc88b3651cd1fe224db6
Siddhant Arora <siddaroraleo@gmail.com>
2026-06-12 12:48:15 -0600

    /link: Wire calibration.py and speed.py to utilize computed scale

--- src/speed.py
+++ src/speed.py
@@ -2,9 +2,10 @@
 # pixel_distance * fps / frame_gap = pixel_speed
 
 import cv2
+import math
 from ultralytics import YOLO
 from pipeline import run_detection
-import math
+from calibration import compute_scale
 
 def compute_speeds(frames, fps, meters_per_pixel=1.0):
     speeds = []
@@ -22,6 +23,10 @@ def compute_speeds(frames, fps, meters_per_pixel=1.0):
 
 if __name__ == "__main__":
     model = YOLO("yolov8n.pt")
+    point1 = (100, 400)
+    point2 = (700, 400)
+    distance_m = 7.32
+    meters_per_pixel = compute_scale(point1, point2, distance_m)
     frames, fps = run_detection("data/input/velocity_test.mp4", "data/output/output_test.mp4", model, 0.25)
-    speeds = compute_speeds(frames, fps)
+    speeds = compute_speeds(frames, fps, meters_per_pixel)
     print(max(speeds, key = lambda x: x[1]))
