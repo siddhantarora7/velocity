7f62644b54118e21335b52c268d466eafae7b629
Siddhant Arora <siddaroraleo@gmail.com>
2026-06-15 21:33:39 -0600

    Add if __name__ == __main__ block to @tracking.py, update requirements.txt to avoud conflicts

--- src/tracking.py
+++ src/tracking.py
@@ -2,8 +2,11 @@
 # Kalman filter
 
 import numpy as np
+from ultralytics import YOLO
 from filterpy.kalman import KalmanFilter
-from filterpy.kalman import Q_discrete_white_noise
+from filterpy.common import Q_discrete_white_noise
+from pipeline import run_detection
+
 
 def clean(frames, fps):
     dt = 1 / fps
@@ -14,6 +17,7 @@ def clean(frames, fps):
     kf.F = np.array([[1, 0, dt, 0], [0, 1, 0, dt], [0, 0, 1, 0], [0, 0, 0, 1]])
     kf.H = np.array([[1, 0, 0, 0], [0, 1, 0, 0]])
     kf.R = np.eye(2) * 9 # measurement noise
+    kf.Q = Q_discrete_white_noise(dim = 2, dt = dt, var = 20.0, block_size = 2)
     kf.P = np.diag([10, 10, 1000, 1000])
 
     # Start at first frame, where v = 0
@@ -25,6 +29,15 @@ def clean(frames, fps):
         kf.predict() # Run the Kalman Filter!
         if cx is not None:
             kf.update([cx, cy])
-        out.append((idx, float(kf.x[0]), float(kf.x[1])))
+        res.append((idx, float(kf.x[0]), float(kf.x[1])))
     
-    return out
+    return res
+
+if __name__ == "__main__":
+    model = YOLO("yolov8n.pt")
+    frames, fps = run_detection("data/input/velocity_test.mp4", "data/output/output_test.mp4", model, 0.25)
+    smooth = clean(frames, fps)
+    
+    # Test
+    print(f"input frames: {len(frames)}, output frames: {len(smooth)}")
+    print(smooth[:5])
