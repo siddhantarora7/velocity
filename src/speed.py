d91c4aaadcd440a2e6cef72851e76da6ea40eb35
Siddhant Arora <siddaroraleo@gmail.com>
2026-06-20 18:07:49 -0600

    Redesign speed.py computation logic

--- src/speed.py
+++ src/speed.py
@@ -9,7 +9,7 @@ from pipeline import run_detection
 from calibration import compute_scale
 
 
-def compute_speeds(frames, fps, meters_per_pixel=1.0):
+def compute_speeds(frames, fps, meters_per_pixel=1.0, window = None):
     speeds, t, xs, ys = [], [], [], []
     for i in range(len(frames) - 1):
         center1, center2 = frames[i], frames[i + 1]
@@ -19,18 +19,17 @@ def compute_speeds(frames, fps, meters_per_pixel=1.0):
         distance = math.sqrt((cx1 - cx2)**2 + (cy1 - cy2)**2) # Euclidean distance
         pixel_speed = distance / time
         real_speed = pixel_speed * meters_per_pixel * 3.6 # Returns km/h
-        if real_speed >= 10:
-            t.append(idx2 / fps)
-            xs.append(cx2)
-            ys.append(cy2)
         speeds.append((idx2 / fps, real_speed))
     
-    if len(t) < 3:
-        launch_speed = None
-    else:
-        a, b, c = np.polyfit(t, ys, 2)
-        vx0, vy0 = np.polyfit(t, xs, 1)[0], 2*a*t[0] + b
-        launch_speed = math.sqrt(vx0**2 + vy0**2) * meters_per_pixel * 3.6
+    launch_speed = None
+    if window is not None:
+        start, end = window
+        seg = frames[start:end + 1]
+        if end - start + 1 >= 3:
+            t, xs, ys = [f[0] / fps for f in seg], [f[1] for f in seg], [f[2] for f in seg]
+            a, b, c = np.polyfit(t, ys, 2) # Get the quadratuc for ball trajectory
+            vx0, vy0 = np.polyfit(t, xs, 1)[0], 2 * a * t[0] + b
+            launch_speed = math.sqrt(vx0*vx0 + vy0*vy0) * meters_per_pixel * 3.6 #km/h
     
     return {"speeds": speeds, "launch": launch_speed}
 
