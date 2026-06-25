dfffd4f9985c41ea1e2891152f17bfe70081b133
Siddhant Arora <siddaroraleo@gmail.com>
2026-06-25 17:03:20 -0600

    Add JSON Web Token (JWT) functionality to auth.py

--- src/auth.py
+++ src/auth.py
@@ -12,9 +12,21 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 ALG = "HS256"
 EXPIRE_MIN = 60*24*7 # 1 week
 
-def hash_password(raw):
+def hash_password(raw: str) -> str:
     return pwd_context.hash(raw)
 
-def verify_password(raw, hashed):
+def verify_password(raw: str, hashed: str) -> bool:
     return pwd_context.verify(raw, hashed)
+
+def create_token(user_id: int) -> str:
+    expire = datetime.now() + timedelta(minutes = EXPIRE_MIN)
+    data = {"sub": str(user_id), "exp": expire}
+    return jwt.encode(payload, SECRET_KEY, algorithm = ALG)
+
+def decode_token(token: str) -> int | None:
+    try:
+        data = jwt.decode(token, SECRET_KEY, algorithms = [ALG])
+        return int(payload["sub"])
+    except Error:
+        return None
 
