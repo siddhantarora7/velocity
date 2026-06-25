53f1e8f50175ddf9c5f4e66ffcec7c5d1effe7e1
Siddhant Arora <siddaroraleo@gmail.com>
2026-06-25 16:58:36 -0600

    Encode and Decode functions

--- src/auth.py
+++ src/auth.py
@@ -11,3 +11,10 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALG = "HS256"
 EXPIRE_MIN = 60*24*7 # 1 week
+
+def hash_password(raw):
+    return pwd_context.hash(raw)
+
+def verify_password(raw, hashed):
+    return pwd_context.verify(raw, hashed)
+
